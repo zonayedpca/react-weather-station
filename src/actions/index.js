e8da244171259320f0565cef322d57411ccd49dc
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-11-13 21:52:30 +0530

    https request added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '45b52714817a2a57145a20af5407037e';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
