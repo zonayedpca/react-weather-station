1b148b6b81e898d8bfb9abb9b7335fc4479d8dc8
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-11-13 21:46:26 +0530

    live deployed

---
+++ precache-manifest.1a3c821005a7ee4e57f11b2bff47348a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "005535e48db0fb2e50bc",
+    "url": "/static/js/main.005535e4.chunk.js"
+  },
+  {
+    "revision": "b51cd8dca66046784d17",
+    "url": "/static/js/1.b51cd8dc.chunk.js"
+  },
+  {
+    "revision": "005535e48db0fb2e50bc",
+    "url": "/static/css/main.6c41c926.chunk.css"
+  },
+  {
+    "revision": "59b3ea9b6aa222e7fe7de51771859f08",
+    "url": "/index.html"
+  }
+];
--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import WeatherReducer from './reducer_weather';
-const rootReducer = combineReducers({
-	weather: WeatherReducer
-});
-
-export default rootReducer;
