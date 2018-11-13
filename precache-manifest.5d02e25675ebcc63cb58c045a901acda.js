db0567c7f52b75ffe063dc4cf4a3dadad60fc1d5
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-11-13 21:49:31 +0530

    homepage added

---
+++ precache-manifest.5d02e25675ebcc63cb58c045a901acda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d292abe994d2fda9ff8",
+    "url": "/react-weather-station/static/js/runtime~main.6d292abe.js"
+  },
+  {
+    "revision": "73cc1c67e71c99e06279",
+    "url": "/react-weather-station/static/js/main.73cc1c67.chunk.js"
+  },
+  {
+    "revision": "b51cd8dca66046784d17",
+    "url": "/react-weather-station/static/js/1.b51cd8dc.chunk.js"
+  },
+  {
+    "revision": "73cc1c67e71c99e06279",
+    "url": "/react-weather-station/static/css/main.6325211f.chunk.css"
+  },
+  {
+    "revision": "534b1ea169eb8d2d2415fbe30355eaa1",
+    "url": "/react-weather-station/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1a3c821005a7ee4e57f11b2bff47348a.js"
+  "/react-weather-station/precache-manifest.5d02e25675ebcc63cb58c045a901acda.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-weather-station/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
