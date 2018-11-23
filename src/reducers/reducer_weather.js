0abb1c4686ea07d057aa76296384b91819a6b94a
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-11-23 10:03:42 +0530

    Update reducer_weather.js

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,7 +1,7 @@
 import { FETCH_WEATHER } from '../actions/index';
 
 // a reducer returns a piece of a state, it receives an action from the action creator
-export default function(state = [], action) {
+export default function(state = [], action = null) {
 	switch (action.type) {
 		case FETCH_WEATHER:
 			return [action.payload.data, ...state];
